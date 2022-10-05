2b20c3e952752d89979f119af5efcd2e9fa02ccf
Julien Labatut <skoognuts@outlook.fr>
2022-10-05 12:49:59 +0200

    Add admin filters

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(AdministratorRepository $administratorRepository, PartnerRepository $partnerRepository, StructureRepository $structureRepository, Request $request): Response
+    public function index(AdministratorRepository $administratorRepository, PartnerRepository $partnerRepository, StructureRepository $structureRepository): Response
     {
         $currentUser = $this->getUser();
         $admin = $administratorRepository->findOneBy(array('userId' => $currentUser->getId()));
@@ -21,10 +21,99 @@ class AdminController extends AbstractController
         $partners = $partnerRepository->findAll();
         $structures = $structureRepository->findAll();
 
+        $activPartnerLinks = [true, false, false];
+        $activStructureLinks = [true, false, false];
+
         return $this->render('admin/index.html.twig', [
             'admin' => $admin,
             'partners' => $partners,
-            'structures' => $structures
+            'structures' => $structures,
+            'activPartnerLinks' => $activPartnerLinks,
+            'activStructureLinks' => $activStructureLinks
+        ]);
+    }
+
+    #[Route('/admin/activ-partners', name: 'app_admin_activ_partners')]
+    public function index_activ_partners(AdministratorRepository $administratorRepository, PartnerRepository $partnerRepository, StructureRepository $structureRepository): Response
+    {
+        $currentUser = $this->getUser();
+        $admin = $administratorRepository->findOneBy(array('userId' => $currentUser->getId()));
+
+        $partners = $partnerRepository->findBy(array('isActive' => true));
+        $structures = $structureRepository->findAll();
+
+        $activPartnerLinks = [false, true, false];
+        $activStructureLinks = [true, false, false];
+
+        return $this->render('admin/index.html.twig', [
+            'admin' => $admin,
+            'partners' => $partners,
+            'structures' => $structures,
+            'activPartnerLinks' => $activPartnerLinks,
+            'activStructureLinks' => $activStructureLinks
+        ]);
+    }
+
+    #[Route('/admin/innactiv-partners', name: 'app_admin_innactiv_partners')]
+    public function index_innactiv_partners(AdministratorRepository $administratorRepository, PartnerRepository $partnerRepository, StructureRepository $structureRepository): Response
+    {
+        $currentUser = $this->getUser();
+        $admin = $administratorRepository->findOneBy(array('userId' => $currentUser->getId()));
+
+        $partners = $partnerRepository->findBy(array('isActive' => false));
+        $structures = $structureRepository->findAll();
+
+        $activPartnerLinks = [false, false, true];
+        $activStructureLinks = [true, false, false];
+
+        return $this->render('admin/index.html.twig', [
+            'admin' => $admin,
+            'partners' => $partners,
+            'structures' => $structures,
+            'activPartnerLinks' => $activPartnerLinks,
+            'activStructureLinks' => $activStructureLinks
+        ]);
+    }
+
+    #[Route('/admin/activ-structures', name: 'app_admin_activ_structures')]
+    public function index_activ_structures(AdministratorRepository $administratorRepository, PartnerRepository $partnerRepository, StructureRepository $structureRepository): Response
+    {
+        $currentUser = $this->getUser();
+        $admin = $administratorRepository->findOneBy(array('userId' => $currentUser->getId()));
+
+        $partners = $partnerRepository->findAll();
+        $structures = $structureRepository->findBy(array('isActive' => true));
+
+        $activPartnerLinks = [true, false, false];
+        $activStructureLinks = [false, true, false];
+
+        return $this->render('admin/index.html.twig', [
+            'admin' => $admin,
+            'partners' => $partners,
+            'structures' => $structures,
+            'activPartnerLinks' => $activPartnerLinks,
+            'activStructureLinks' => $activStructureLinks
+        ]);
+    }
+
+    #[Route('/admin/innactiv-structures', name: 'app_admin_innactiv_structures')]
+    public function index_innactiv_structures(AdministratorRepository $administratorRepository, PartnerRepository $partnerRepository, StructureRepository $structureRepository): Response
+    {
+        $currentUser = $this->getUser();
+        $admin = $administratorRepository->findOneBy(array('userId' => $currentUser->getId()));
+
+        $partners = $partnerRepository->findAll();
+        $structures = $structureRepository->findBy(array('isActive' => false));
+
+        $activPartnerLinks = [true, false, false];
+        $activStructureLinks = [false, false, true];
+
+        return $this->render('admin/index.html.twig', [
+            'admin' => $admin,
+            'partners' => $partners,
+            'structures' => $structures,
+            'activPartnerLinks' => $activPartnerLinks,
+            'activStructureLinks' => $activStructureLinks
         ]);
     }
 
